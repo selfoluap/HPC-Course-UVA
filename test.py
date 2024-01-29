5fa6ec356145560c89083b9bbd83eabbca4ea68b
Paulo Liedtke <liedtke.paulo@gmail.com>
2024-01-29 19:30:36 +0100

    removed test gof version

--- test.py
+++
@@ -1,8 +0,0 @@
-with open("test.txt", "r") as f:
-    #read lines and remove { and }
-    lines = [line.strip("{}") for line in f.readlines()]
-    count = 0
-    for line in lines:
-        #count number of occurrences of 1
-        count += line.count("1")
-    print(count)
