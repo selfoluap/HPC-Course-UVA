0e9dfb8bcc934f94799e515f9dbfc3aa25b8c112
Paulo Liedtke <liedtke.paulo@gmail.com>
2024-02-01 15:48:42 +0100

    asd

--- Assignment MPI and OpenMP/openMP/simple_job.sh
+++ Assignment MPI and OpenMP/openMP/simple_job.sh
@@ -12,12 +12,4 @@ module load 2023
 module load GCC/12.3.0
 module load OpenMPI/4.1.5-GCC-12.3.0
 
-echo "OpenMP parallelism"
-for i in 128 64 32
-do
-export OMP_NUM_THREADS=$i
-echo "CPUS: " $OMP_NUM_THREADS
-echo "CPUS: " $OMP_NUM_THREADS >&2
-./gof 0 > gof_$i.out
-done
-echo "DONE "
+./low_grid 0 > gof_128.out
